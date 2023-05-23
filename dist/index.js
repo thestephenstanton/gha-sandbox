fcb0c25f72bfe3e5008921b568d4d30eb65c1136
thestephenstanton <stephenstanton10@gmail.com>
2023-05-23 14:52:50 -0400

    update

--- dist/index.js
+++ dist/index.js
@@ -9792,32 +9792,42 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(4181);
 const github = __nccwpck_require__(2726);
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
-    console.log("getting token")
-    const githubToken = core.getInput("GITHUB_TOKEN")
-
-    console.log("lenght of token", githubToken.length)
-
-    const octokit = github.getOctokit(githubToken)
-
-    octokit.rest.issues.createComment({
-        ...github.context.repo,
-        issue_number: github.context.issue.number,
-        body: "Hello from Octokit!"
-    })
-
-} catch (error) {
-    console.log("ah fuck")
-  core.setFailed(error.message);
+async function run() {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const nameToGreet = core.getInput('who-to-greet');
+        console.log(`Hello ${nameToGreet}!`);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        // Get the JSON webhook payload for the event that triggered the workflow
+      //   const payload = JSON.stringify(github.context.payload, undefined, 2)
+      //   console.log(`The event payload: ${payload}`);
+          console.log("getting token")
+          const githubToken = core.getInput("GITHUB_TOKEN")
+      
+          console.log("lenght of token", githubToken.length)
+      
+          const octokit = github.getOctokit(githubToken)
+      
+          await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
+              ...github.context.repo,
+              issue_number: github.context.issue.number,
+              body: "Hello from Octokit!"
+          });
+      
+          // octokit.rest.issues.createComment({
+          //     ...github.context.repo,
+          //     issue_number: github.context.issue.number,
+          //     body: "Hello from Octokit!"
+          // })
+      
+      } catch (error) {
+          console.log("ah fuck")
+        core.setFailed(error.message);
+      }
 }
+
+run()
 })();
 
 module.exports = __webpack_exports__;
--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
-    console.log("getting token")
-    const githubToken = core.getInput("GITHUB_TOKEN")
-
-    console.log("lenght of token", githubToken.length)
-
-    const octokit = github.getOctokit(githubToken)
-
-    octokit.rest.issues.createComment({
-        ...github.context.repo,
-        issue_number: github.context.issue.number,
-        body: "Hello from Octokit!"
-    })
-
-} catch (error) {
-    console.log("ah fuck")
-  core.setFailed(error.message);
+async function run() {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const nameToGreet = core.getInput('who-to-greet');
+        console.log(`Hello ${nameToGreet}!`);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        // Get the JSON webhook payload for the event that triggered the workflow
+      //   const payload = JSON.stringify(github.context.payload, undefined, 2)
+      //   console.log(`The event payload: ${payload}`);
+          console.log("getting token")
+          const githubToken = core.getInput("GITHUB_TOKEN")
+      
+          console.log("lenght of token", githubToken.length)
+      
+          const octokit = github.getOctokit(githubToken)
+      
+          await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
+              ...github.context.repo,
+              issue_number: github.context.issue.number,
+              body: "Hello from Octokit!"
+          });
+      
+          // octokit.rest.issues.createComment({
+          //     ...github.context.repo,
+          //     issue_number: github.context.issue.number,
+          //     body: "Hello from Octokit!"
+          // })
+      
+      } catch (error) {
+          console.log("ah fuck")
+        core.setFailed(error.message);
+      }
 }
+
+run()
