83ace87b2e256200380ac715f0c703f1d4fa4398
thestephenstanton <stephenstanton10@gmail.com>
2023-05-23 14:43:08 -0400

    update

--- dist/index.js
+++ dist/index.js
@@ -9805,9 +9805,6 @@ try {
     const githubToken = core.getInput("GITHUB_TOKEN")
 
     console.log("lenght of token", githubToken.length)
-    console.log("ok NOW here ")
-    console.log("issue number", github.context.issue.number)
-    console.log("repo", github.context.repo)
 
     const octokit = github.getOctokit(githubToken)
 
