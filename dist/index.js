4d58b3f4777b5933ffe77e10e99a9e4fff573a55
thestephenstanton <stephenstanton10@gmail.com>
2023-05-23 14:45:02 -0400

    update

--- dist/index.js
+++ dist/index.js
@@ -9807,6 +9807,10 @@ try {
     console.log("lenght of token", githubToken.length)
 
     const octokit = github.getOctokit(githubToken)
+    
+    console.log("octokit", octokit)
+    console.log("octokit issues", octokit.issues)
+
 
     octokit.issues.createComment({
         ...github.context.repo,
